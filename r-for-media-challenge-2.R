c3e5b499487681d584db3174fcb35c098deab26d
Philipp Kessling <32732590+pekasen@users.noreply.github.com>
2021-04-06 15:46:36 +0200

    Update r-for-media-challenge-2.R

--- r-for-media-challenge-2.R
+++ r-for-media-challenge-2.R
@@ -3,7 +3,7 @@ library(assertthat)
 # Calculate the population for every district for 2021 and name the new column einwohner_2021. The yearly increasing rate is 1.1%. Note: the data is based on a survey from 2019.
 # * Store the result in the object hamburg_2021
 # * Round the population for 2021
-# * Calculate the difference in population between 2019 and 2020 and name the new column differenz
+# * Calculate the difference in population between 2019 and 2021 and name the new column differenz
 # * Filter for all districts with a population increase of more than 5000 people
 # * Select bezirk and differenz
 # * Arrange the output in descending order.
