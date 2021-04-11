a6cf32bccac91262f58cdbf857e6b5068873b3b7
Fred Nasser <fred.nasser@haw-hamburg.de>
2021-04-11 21:20:34 +0200

    Nasser challenge 2

--- r-for-media-challenge-2.R
+++ r-for-media-challenge-2.R
@@ -9,6 +9,30 @@ library(assertthat)
 # * Arrange the output in descending order.
 # * Calculate the sum of people who moved to Hamburg between 2019 and 2021 and store the result in the object hamburg_pop_increase and name the column differenz_sum
 
+bezirk = c("Hamburg-Mitte", "Altona", "Eimsbüttel", "Hamburg-Nord", "Wandsbek", "Bergedorf", "Harburg") 
+einwohner = c(301543, 275264, 267051, 314593, 441012, 130260, 169426)
+bevoelkerungsdichte = c(2121, 3534, 5362, 5443, 2990, 841, 1353)
+bezirksamtsleiter = c("Falko Droßmann (SPD)", "Stefanie von Berg (Grüne)", "Kay Gätgens (SPD)", "Michael Werner-Boelz (Grüne)", "Thomas Ritzenhoff (SPD)", "Arne Dornquast (SPD)", "Sophie Fredenhagen (parteilos)")
+flaeche = c(TRUE, FALSE, FALSE, FALSE, TRUE, TRUE, TRUE) 
+hamburg_df <- data.frame(bezirk, einwohner, bevoelkerungsdichte, bezirksamtsleiter, flaeche)
+
+library(dplyr)
+
+hamburg <- as_tibble(hamburg_df)
+
+# hamburg_2021
+hamburg_2021 <- hamburg %>% 
+  mutate(einwohner_2021= round(einwohner* 1.011^2)) %>% 
+  mutate(differenz = einwohner_2021 - einwohner) %>% 
+  filter(differenz > 5000) %>% 
+  select(bezirk, differenz) %>% 
+  arrange(desc(differenz))
+
+# hamburg_pop_increase     
+hamburg_pop_increase <- hamburg_2021 %>% 
+  summarise(differenz_sum = sum(differenz))
+
+
 if(
   assert_that(
     has_name(hamburg_2021, "bezirk"),
